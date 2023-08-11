d709b2b37387e96aa62856759e2ce78b34db16bb
sonsungjong <tjdwhd70599@gmail.com>
2023-08-12 08:11:02 +0900

    STT_TTS 샘플

---
+++ app/src/main/java/com/example/stt_tts_app/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.stt_tts_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button hello_btn;
+    TextView textView1;
+    TextToSpeech tts;
+    Button go_stt_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        hello_btn = findViewById(R.id.hello_btn);
+        textView1 = findViewById(R.id.textView1);
+        go_stt_btn = findViewById(R.id.go_stt_btn);
+
+        // tts 생성
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener(){
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR){
+                    tts.setLanguage(Locale.KOREA);
+                }
+            }
+        });
+
+        // hello_btn 클릭이벤트 추가
+        hello_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView1.setText("안녕하세요 반갑습니다.");
+
+                // tts 사용
+                Bundle params = new Bundle();
+                params.putString(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "");
+                tts.speak("안녕하세요 반갑습니다.", TextToSpeech.QUEUE_FLUSH, params, "UniqueID");
+            }
+        });
+
+        // go_stt_btn 클릭이벤트 추가
+        go_stt_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SttActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/stt_tts_app/SttActivity.java
@@ -0,0 +1,146 @@
+package com.example.stt_tts_app;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import android.Manifest;
+
+public class SttActivity extends AppCompatActivity {
+
+    private TextView stt_text;
+    private Button stt_button;
+    private Button go_tts_button;
+    private SpeechRecognizer speech_recognizer;
+    private Intent recognizer_intent;
+    private boolean flag = false;
+
+    private static final int REQUEST_CODE = 200; // 권한 요청 코드, 임의의 값
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stt);
+
+        stt_text = findViewById(R.id.stt_text);
+        stt_button = findViewById(R.id.stt_button);
+        go_tts_button = findViewById(R.id.go_tts_btn);
+        speech_recognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        recognizer_intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizer_intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        recognizer_intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREAN);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_CODE);
+        }
+
+
+        speech_recognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+                Log.e("SpeechRecognition", "Error code: " + i);
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (matches != null && !matches.isEmpty()) {
+                    stt_text.setText(stt_text.getText().toString() + "\n" + matches.get(0));
+                } else {
+                    stt_text.setText(stt_text.getText().toString() + "\n" + "Result의 matches오류");
+                }
+
+                // 다시 녹음 시작 (flag가 true일 때)
+                if (flag == true) {
+                    speech_recognizer.startListening(recognizer_intent);
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+                stt_text.setText(stt_text.getText().toString()+"\n"+"부분 Results");
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        stt_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (flag == false) {
+                    speech_recognizer.startListening(recognizer_intent);
+                    stt_button.setText("Stop");
+                    flag = true;
+                } else {
+                    speech_recognizer.stopListening();
+                    stt_button.setText("Start");
+                    flag = false;
+                }
+            }
+        });
+
+        go_tts_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SttActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(speech_recognizer != null){
+            speech_recognizer.destroy();
+        }
+    }
+}
